e23b318ba91d941f7137ef069b27268279a81788
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-11 13:05:20 +0300

    Добавьте файлы проекта.

---
+++ zadanie 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задание 2 ");
+        AllInfoProcess();
+        Console.ReadKey();
+    }
+
+    static void AllInfoProcess()
+    {
+        var myProcess = from proc in Process.GetProcesses(".")
+                        orderby proc.Id
+                        select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n");
+        foreach (var p in myProcess)
+            Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);  
+        Console.WriteLine("Первые 3 процесса");
+        int id = 0;
+        foreach (var p in myProcess)
+            if (id < 3)
+            {
+                Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+                id++;
+            }
+    }
+}
